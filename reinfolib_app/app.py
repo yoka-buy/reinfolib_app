3136d8c3e3e86e6320958ba9e55d3afc6cea3e29
DSE <ds.estate.fukuoka@gmail.com>
2024-07-08 19:56:45 +0900

    inject ga

--- reinfolib_app/app.py
+++ reinfolib_app/app.py
@@ -25,11 +25,45 @@ import streamlit as st
 import polars as pl
 import folium
 from streamlit_folium import st_folium
+from bs4 import BeautifulSoup
+import pathlib
+import shutil
 
 
 @st.cache_data
 def read_data():
     return pl.read_csv("data/data.csv")
+
+
+GA_ID = "google_analytics"
+GA_SCRIPT = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J3NFT5N753"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J3NFT5N753');
+</script>
+"""
+
+
+def inject_ga():
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):
+        bck_index = index_path.with_suffix(".bck")
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)
+        else:
+            shutil.copy(index_path, bck_index)
+        html = str(soup)
+        new_html = html.replace("<head>", "<head>\n" + GA_SCRIPT)
+        index_path.write_text(new_html)
+
+
+inject_ga()
 
 
 df = read_data()
@@ -38,7 +72,7 @@ price_category_list = df["PriceCategory"].unique().sort(descending=True).to_list
 period_list = df["Period"].unique().sort(descending=True).to_list()
 prefecture_list = df["Prefecture"].unique().sort().to_list()
 
-st.header("成約坪単価Viewer")
+st.header("中古マンション成約坪単価Viewer")
 
 
 with st.expander("絞り込み条件", expanded=True):
